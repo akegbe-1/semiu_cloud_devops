9d589468f54f73d31a9902bbc9dd66d7823e315c
Semiu Omotola Akegbeyale <semiuakegbeyale@gmail.com>
2026-04-02 10:45:34 +0100

    Add AWS access and secret keys to main.tf
    Added access and secret keys for AWS provider.

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
     region = "us-west-2"
+access_key = hclfi3fu
+secret_key = yfyfy8f338
  resource "aws_instance" "example" {
         ami           = "ami-0c55b159cbfafe1f0"
         instance_type = "t2.micro"
